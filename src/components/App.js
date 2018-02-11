1b2814276bf6fb44a543da0d73d095783a704f9d
Manjula Dube <dube.manjula668@gmail.com>
2018-02-11 14:18:13 +0530

    gql and graphql

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 import logo from '../logo.svg';
 import '../styles/App.css';
+
+
+const ChannelsList = ({ data: {loading, error, channels }}) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return <ul>
+    { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
+  </ul>;
+};
+
+const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+
 
 const HelloWorld = () =>
      (<ul>
