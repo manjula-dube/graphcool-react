685f5614f2cc79c4f87c3f3900f6c9d4927f3c08
Manjula Dube <dube.manjula668@gmail.com>
2018-02-11 14:05:17 +0530

    move everything to index folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-const client = new ApolloClient({
-  // By default, this client will send queries to the
-  //  `/graphql` endpoint on the same host
-  link: new HttpLink(),
-  cache: new InMemoryCache()
-});
 
 const HelloWorld = () =>
      (<ul>
